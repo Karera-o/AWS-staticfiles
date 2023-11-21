c2337638be567b2a92e757128ea06c002288e9bd
csrolivier <okarera@compstaffing.com>
2023-11-21 19:14:52 +0200

    wip

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wq3j%s#%yig$k@#-760%e^a@kh(_pqco@x^&*d#jr#u&an$t6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','inyambo.onrender.com']
 
 
 # Application definition
