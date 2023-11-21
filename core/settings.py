423e525cde957e26a4a63b0a70ae6ef1543e37d5
csrolivier <okarera@compstaffing.com>
2023-11-21 19:41:36 +0200

    wip

--- core/settings.py
+++ core/settings.py
@@ -172,8 +172,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    # DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
+    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # DEFFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
     # ...
